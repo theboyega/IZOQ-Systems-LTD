d2d71315d81bc9c8242745bc23fc2c7f9861a046
Adeyemi Tomiwa <91728341+theboyega@users.noreply.github.com>
2026-02-25 13:55:59 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "izoq-landing",
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist",
-  "framework": "vite",
-  "rewrites": [
-    { "source": "/(.*)", "destination": "/index.html" }
-  ]
-}
